02ac8bb08774bd5408d90a8f754269b334766fe7
Jeff Peters <petersjeff288@gmail.com>
2020-05-10 17:55:33 -0400

    todays updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getNumberOfCurrencyDigits } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,33 +8,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+  box1: number;
+  box2: number;
+  results:number;
 
-}
+  constructor( ){ };
 
+  }
+
+let box1 = document.querySelector("#box1");
+console.log(box1);
+let box2 = Number(document.getElementById("box2"));
+console.log(box2);
 let results:number =0;
 
-const add = (box1:number, box2:number) => {
-   results = box1 + box2;
-   display(results);
+function add(box1, box2): number {
+  let results:number = box1 + box2;
+  console.log(results);
+  return results;
+   // display(results);
 }
 
-const subt = (box1:number, box2:number) => {
-  results = box1 - box2;
-  display(results);
+
+// const subt = (box1:number, box2:number) => {
+//   results = box1 - box2;
+//   display(results);
+// }
+
+// const mult =(box1:number, box2:number) => {
+//   results = box1 * box2;
+//   display(results);
+// }
+
+// const divi = (box1:number, box2:number) => {
+//   results = box1 / box2;
+//   display(results);
+// }
+
+const display = (results: number) => {
+  let rts = document.querySelector("#results");
+  rts.innerHTML = "results";
+
+  //document.getElementById("results").value = results;
+
 }
 
-const mult =(box1:number, box2:number) => {
-  results = box1 * box2;
-  display(results);
-}
-
-const divi = (box1:number, box2:number) => {
-  results = box1 / box2;
-  display(results);
-}
-
-const display = (results:number) => {
-  let nbrCtrl = document.getElementById("results");
-  nbrCtrl.value = results;
-
-}
+// const loaded = () =>{
+//   display(results);
+// }
