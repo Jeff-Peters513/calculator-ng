1ad92beeb1dae241f02324138452afd4b84522fa
Jeff Peters <petersjeff288@gmail.com>
2020-05-09 21:56:48 -0400

    modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Calculator';
 
+}
 
+let results:number =0;
 
-  
+const add = (box1:number, box2:number) => {
+   results = box1 + box2;
+   display(results);
+}
+
+const subt = (box1:number, box2:number) => {
+  results = box1 - box2;
+  display(results);
+}
+
+const mult =(box1:number, box2:number) => {
+  results = box1 * box2;
+  display(results);
+}
+
+const divi = (box1:number, box2:number) => {
+  results = box1 / box2;
+  display(results);
+}
+
+const display = (results:number) => {
+  let nbrCtrl = document.getElementById("results");
+  nbrCtrl.value = results;
+
 }
