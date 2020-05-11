54be733fa40a2263ab1371f2af8d32ee8e08e9e2
Jeff Peters <petersjeff288@gmail.com>
2020-05-11 14:47:42 -0400

    corrected code and updated html-css-format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,45 +14,18 @@ export class AppComponent {
 
   constructor( ){ };
 
+  add() {
+    this.results = this.box1 + this.box2;
   }
 
-let box1 = document.querySelector("#box1");
-console.log(box1);
-let box2 = Number(document.getElementById("box2"));
-console.log(box2);
-let results:number =0;
-
-function add(box1, box2): number {
-  let results:number = box1 + box2;
-  console.log(results);
-  return results;
-   // display(results);
+  subt() {
+    this.results = this.box1 - this.box2;
+  }
+  mult() {
+    this.results = this.box1 * this.box2;
+  }
+  divi() {
+    this.results = this.box1 / this.box2;
+  }
 }
 
-
-// const subt = (box1:number, box2:number) => {
-//   results = box1 - box2;
-//   display(results);
-// }
-
-// const mult =(box1:number, box2:number) => {
-//   results = box1 * box2;
-//   display(results);
-// }
-
-// const divi = (box1:number, box2:number) => {
-//   results = box1 / box2;
-//   display(results);
-// }
-
-const display = (results: number) => {
-  let rts = document.querySelector("#results");
-  rts.innerHTML = "results";
-
-  //document.getElementById("results").value = results;
-
-}
-
-// const loaded = () =>{
-//   display(results);
-// }
